23a9626b82ffb189812d071502acfdecf5b3e553
PMTpro <pmt.coder@gmail.com>
2022-08-13 14:07:28 +0700

    update

--- manager/checkbox.js
+++
@@ -1,8 +0,0 @@
-function onCheckItem()
-{
-    for (let i = 0; i < document.form.elements.length; ++i) {
-        if (document.form.elements[i].type === 'checkbox') {
-            document.form.elements[i].checked = document.form.all.checked === true;
-        }
-    }
-}
--- manager/version.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    if (!defined('ACCESS')) {
-        die('Not acces');
-    } else {
-        $count   = 5;
-        $version = '1.0';
-    }
